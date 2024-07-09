7a9b3ecd0229837854ecee9200dba1a5daaa2338
VesperSpoony <714694113@qq.com>
2024-07-09 11:14:36 +0800

    性能优化相关api

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+// import App from './App'
+// import App from './useReducer'
+// import App from './useMemo'
+// import App from './memo-基础用法'
+// import App from './memo-props比较机制'
+// import App from './useCallback'
+// import App from './forwardRef'
+// import App from './useInperativeHandle'
+// import App from './class-实现counter'
+// import App from './class-生命周期函数'
+// import App from './class-组件通信'
+// import App from './zustand-基础使用'
+// import App from './zustand-异步支持'
+import App from './zustand-切片模式'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
+
